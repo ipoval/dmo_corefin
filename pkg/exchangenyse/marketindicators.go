ff9f12ce62ccfd25e218d4f230e4b0155ae74cfb
Ivan Povalyukhin <ipovalyukhin@atlassian.com>
2020-12-01 06:40:04 +1100

    added cnn fear and greed index

--- pkg/exchangenyse/marketindicators.go
+++ pkg/exchangenyse/marketindicators.go
@@ -1,11 +1,13 @@
 package exchangenyse
 
 var MarketIndicators = []struct {
-	Symbol string
-	Desc   string
+	Symbol     string
+	Desc       string
+	HostOrigin string
 }{
-	{Symbol: "$SPX", Desc: "S&P500"},
-	{Symbol: "$VIX", Desc: "Volatility index"},
-	{Symbol: "$SPX:$GOLD", Desc: "S&P500/Gold"},
-	{Symbol: "$CPCE", Desc: "Options equity Put/Call ratio"},
+	{Symbol: "$SPX", Desc: "S&P500", HostOrigin: "stockcharts.com"},
+	{Symbol: "$VIX", Desc: "Volatility index", HostOrigin: "stockcharts.com"},
+	{Symbol: "$SPX:$GOLD", Desc: "S&P500/Gold", HostOrigin: "stockcharts.com"},
+	{Symbol: "$CPCE", Desc: "Options equity Put/Call ratio", HostOrigin: "stockcharts.com"},
+	{Symbol: "CNN_FEAR_AND_GREED_INDEX", Desc: "CNN fear and greed indicator", HostOrigin: "money.cnn.com"},
 }
