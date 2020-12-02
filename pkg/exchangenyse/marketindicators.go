7debd44b6c4387e55817a10177a731704a47587d
Ivan Povalyukhin <ipovalyukhin@atlassian.com>
2020-12-03 06:19:58 +1100

    added GDXZ

--- pkg/exchangenyse/marketindicators.go
+++ pkg/exchangenyse/marketindicators.go
@@ -8,6 +8,8 @@ var MarketIndicators = []struct {
 	{Symbol: "$SPX", Desc: "S&P500", HostOrigin: "stockcharts.com"},
 	{Symbol: "$VIX", Desc: "Volatility index", HostOrigin: "stockcharts.com"},
 	{Symbol: "$SPX:$GOLD", Desc: "S&P500/Gold", HostOrigin: "stockcharts.com"},
+	{Symbol: "$GLD", Desc: "Gold shares", HostOrigin: "stockcharts.com"},
+	{Symbol: "!GT50GDX", Desc: "% of GDX above 50dma", HostOrigin: "stockcharts.com"},
 	{Symbol: "$CPCE", Desc: "Options equity Put/Call ratio", HostOrigin: "stockcharts.com"},
 	{Symbol: "CNN_FEAR_AND_GREED_INDEX", Desc: "CNN fear and greed indicator", HostOrigin: "money.cnn.com"},
 }
